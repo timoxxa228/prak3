9a96032b4628f29fb45534d5a564d99e325eb808
timoxxa228 <timoysha10@gmail.com>
2023-03-24 11:11:49 +0200

    Add project files.

---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double kurs1, kurs2, suma, sum_out;
+            kurs1 = double.Parse(textBox1.Text);
+            kurs2 = double.Parse(textBox2.Text);
+            if (radioButton1.Checked - true)
+        }
+        textBox4.Enabled = true;
+        suma= double.Parse(textBox3.Text);
+        sum_out - suma * kursi;
+        textBox4.Text = sum_out.ToString();
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            label2.Text = "-"
+            textBox3.Enabled = false;
+            textBox4.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string S = textBox1.Text:
+            int a = int.Parse(S);
+            int b = a * a;
+            Lable1.Text = b.ToString();
+
+        }
+    }
+}
